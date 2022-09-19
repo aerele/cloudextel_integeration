abfae811c076a3004f67530c8d325e664feb4f54
DHINESH00 <18csa09@karpagamtech.ac.in>
2022-09-19 20:35:16 +0530

    refactor[create_purchasereceipt]: create purchase receipt with reference to connector purchase and purchase order

--- cloudextel_integeration/cloudextel_integeration/doctype/connector_purchase_receipt/connector_purchase_receipt.js
+++ cloudextel_integeration/cloudextel_integeration/doctype/connector_purchase_receipt/connector_purchase_receipt.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Connector Purchase Receipt', {
-	// refresh: function(frm) {
-
-	// }
+	refresh:function(frm){
+		frm.add_custom_button(__("Create Purchase Receipt"), function(){
+			if(frm.doc.sync == 1){
+				frappe.msgprint('Purchase Receipt Already Created')
+				return
+			}
+			frappe.call({
+				'method': 'cloudextel_integeration.cloudextel_integeration.doctype.connector_purchase_receipt.connector_purchase_receipt.new_purchasereceipt',
+				
+				'args':{
+				'purchase_receipt_id': frm.doc.name
+				},
+				'callback':function(res){
+					if(res.message){
+						frappe.msgprint("Purchase Receipt created")
+					}
+				}
+			})
+		})
+	}
 });
--- cloudextel_integeration/cloudextel_integeration/doctype/connector_purchase_receipt/connector_purchase_receipt.py
+++ cloudextel_integeration/cloudextel_integeration/doctype/connector_purchase_receipt/connector_purchase_receipt.py
@@ -1,8 +1,41 @@
 # Copyright (c) 2022, CloudExtel and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from erpnext.buying.doctype.purchase_order.purchase_order import make_purchase_receipt
 class ConnectorPurchaseReceipt(Document):
 	pass
+@frappe.whitelist()
+def create_purchasereceipt():
+	orders = frappe.db.sql("""select name  from `tabConnector Purchase Receipt` where  is_synced =0 and retry_limit>0 order by creation desc limit 20""",as_dict=1)
+	for purchase_receipt_id in orders:
+		new_purchasereceipt(purchase_receipt_id["name"])
+@frappe.whitelist()
+def new_purchasereceipt(purchase_receipt_id):
+	c_purchase_receipt=frappe.get_doc("Connector Purchase Receipt",purchase_receipt_id)
+	reference=frappe.db.sql("""select count(reference_no) from `tabPurchase Receipt` where reference_no= %s """,(c_purchase_receipt.reference_no),as_list=1)
+	if reference[0][0]==0:
+		frappe.db.sql("""update `tabConnector Purchase Receipt` set retry_limit=retry_limit-1 where name= %s """,(purchase_receipt_id))
+		purchase_receipt=make_purchase_receipt(c_purchase_receipt.purchase_order_no)
+		c_item = {}
+		p_item=[]
+		for i in c_purchase_receipt.items:
+			c_item[i.item_code] = i
+		for item in (purchase_receipt.items):
+			if	item.item_code in c_item:
+				c_it = c_item[item.item_code]
+				item.received_stock_qty= c_it.received_stock_qty
+				item.stock_qty=c_it.stock_qty
+				item.returned_qty=c_it.returned_qty
+				p_item.append(item)
+		purchase_receipt.items=p_item
+		try:
+			purchase_receipt.save(ignore_permissions=True)
+			frappe.db.sql("""update `tabPurchase Receipt` set reference_no= %s where name= %s""",(c_purchase_receipt.reference_no,purchase_receipt.name))
+			frappe.db.sql("""update `tabConnector Purchase Receipt` set is_synced= 1 where name= %s""",(c_purchase_receipt.name))
+			
+		except Exception as e:
+			frappe.errprint(e)
+	
+	
